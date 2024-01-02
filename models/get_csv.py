b9eed6869526e2734d8981e5778c0a3a4ce5d0e5
Алекс <essepunto@mail.ru>
2024-01-02 11:51:57 +0300

    засунул чат id в dotenv

--- models/get_csv.py
+++ models/get_csv.py
@@ -5,30 +5,28 @@ import sys
 
 import sqlalchemy
 from aiogram import types, Dispatcher
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from create_bot import bot
 from models.db import SkuToSection, engine
 
+# Создаем сессию для подключения к базе данных
 Session = sessionmaker(bind=engine)
 
-async def process_csv_file(file_path):
+async def process_csv_file(file_path, message: types.Message):
     with Session() as session:
         with open(file_path, newline='', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter=';')
-            try:
-                for row in reader:
+            for row in reader:
+                try:
                     sku, section_name = row
                     record = SkuToSection(sku=sku, section_name=section_name)
                     session.add(record)
-
                     session.commit()
-            except sqlalchemy.exc.IntegrityError as e:
-                session.rollback()
-                # Откатываем изменения, если произошла ошибка
-                # Здесь вы можете добавить логику обработки ошибки, например, отправить сообщение пользователю
-                print("Ошибка: элемент с таким ключом уже существует.")
-
+                except sqlalchemy.exc.IntegrityError:
+                    session.rollback()
+                    await message.reply(f"Ошибка: элемент с SKU  уже существует.")
+                    return
 
 async def upload_csv(message: types.Message):
     document_id = message.document.file_id
@@ -39,19 +37,15 @@ async def upload_csv(message: types.Message):
     with open(file_path, "wb") as new_file:
         new_file.write(downloaded_file.getvalue())
 
-    await process_csv_file(file_path)
-
-    os.remove(file_path)  # Удаление временного файла
-
-    await message.reply("CSV файл обработан и данные добавлены в базу данных.")
-
+    await process_csv_file(file_path, message)
+    os.remove(file_path)
+    await message.reply("CSV файл обработан.")
 
 async def run_script(message: types.Message):
-    # Вызов скрипта
     subprocess.run([sys.executable, "db_init.py"])
     await message.reply("Скрипт выполнен.")
-
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(upload_csv, content_types=['document'])
     dp.register_message_handler(run_script, commands=['postgres_init'])
+
